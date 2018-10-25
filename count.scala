60816336a4656a2391f9b17044162aa16da49d99
Preet Kamal <xpreetkamal007@gmail.com>
2018-10-26 00:31:24 +0530

    Update count.scala

--- count.scala
+++ count.scala
@@ -1,5 +1,5 @@
  val text = sc.textFile("saeyum.txt") 
 
- val counts = text.flatMap(line => line.split(" ")
+ val count = text.flatMap(line => line.split(" ")
 
  ).map(word => (word,1)).reduceByKey(_+_) .count()
